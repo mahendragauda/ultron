a5d84cf721f3cb51991bb54e38c8c3aac46139e8
Siddhi_Rane1234 <siddhirane265@gmail.com>
2022-03-07 22:46:04 -0500

    api binding completed in amp/shows

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from "next/image";
 import Link from 'next/link';
 import style from '../styles/amp/Shows.module.css'
+import VideoPlayIcon from "./common/VideoPlayIcon";
 
 
 class StoryList extends React.Component {
@@ -14,33 +15,35 @@ class StoryList extends React.Component {
     }
 
     render() {
-        console.log(this.state.showList)
         return (
             <>
+             {this.state.showList.shows.map((stories, idx) => (
                <div className={style.storyWrapper}>
-                    <Link href="https://www.republicworld.com/amp/shows/nation-wants-to-know/nirmala-sitharaman-speaks-to-arnab-about-indias-economy-takes-on-budget-2022s-critics.html">
+                    <Link href={stories.complete_slug}>
                         <a>
                             <div className={style.hashTagContainer}>
                                 <div className={style.showTitleWrapper}>
                                     <div className={style.showTitleDiv}>
-                                        <div className={style.showTitleText}>Nation wants to know</div>
+                                        <div className={style.showTitleText}>{stories.sub_category_name}</div>
                                     </div>
                                 </div>
                                 <div>
-                                    <h3 className={style.headLine}>#NirmalaSpeaksToArnab</h3>
+                                    <h3 className={style.headLine}>{stories.headline}</h3>
                                 </div>
                             </div>
                             <div className={style.imgContainer}>
-                                <div className={[style.imgDiv,style.progressive].join(" ")}>
-                                    <img src="https://img.republicworld.com/republic-prod/shows/promolarge/hdpi/164408197961feb33b42b1f.jpeg?tr=w-425,h-233"></img>
-                                    </div>
+                                <div className={style.progressive}>
+                                    <img src={stories.placeholder} className={style.imgDiv} />
+                                </div>
+                                <VideoPlayIcon />
                             </div>
                             <div className={style.showDescription}>
-                        <div className={style.descriptionText}>Nirmala Sitharaman speaks to Arnab about India's economy; takes on Budget 2022's critics</div>
+                        <div className={style.descriptionText}>{stories.title}</div>
                     </div>
                         </a>
                     </Link>
                 </div> 
+                 ))}  
             </>
            
         )}
